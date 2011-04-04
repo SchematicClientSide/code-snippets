a8d11be3336254e5dbf606305d81d8c1fb12f27d
jorgeortega <jorgeortega@gmail.com>
2011-04-04 15:04:00 -0600

    adding snippet to make first letter bigger

---
+++ firstLetterBigger.js
@@ -0,0 +1,21 @@
+function firstLetterBigger() {
+    firstParagraphElement = $('.content p:first');
+    firstParagraphText = firstParagraphElement.text();
+    letter = firstParagraphText.substr(0, 1);
+    firstParagraphText = firstParagraphText.substr(1);
+    firstParagraphText = '<div class="firstLetter">' + letter + '</div>' + firstParagraphText;
+    firstParagraphElement.html(firstParagraphText);
+}
+
+/*
+css
+ 
+
+div#post p .firstLetter {
+    font-size: 40px;
+    float: left;
+    width: 34px;
+    height: 25px;
+    margin: 14px 0 0 0;
+}
+*/
